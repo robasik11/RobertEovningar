148cac3e90108a22a28af739315e319fdcc3f374
robasik11 <robasik11@gmail.com>
2020-03-02 19:54:08 +0100

    Första 3 övningarna

---
+++ RobertEovningar/RobertEovningar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RobertEovningar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (3 < 6)
+            {
+                Console.WriteLine("Hello World!");
+            }
+
+            Console.WriteLine("Please write a username..");
+            string name;
+            string password;
+
+            name = Console.ReadLine();
+
+            if (name == "")
+            {
+                Console.WriteLine("Welcome");
+                password = "NoPass";
+            }
+
+            else
+            {
+                Console.WriteLine("Please write a password..");
+                password = Console.ReadLine();
+            }
+
+            Console.WriteLine("Your username is " + name + " and your password is " + password);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
